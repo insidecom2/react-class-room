379b05200cb1be86abd829ce0e09619c21af826d
Jakkrirt <Mona Lisa>
2023-02-07 16:34:31 +0700

    feat : update rewire alias

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,7 +1,8 @@
 import { createBrowserRouter, createRoutesFromElements, Outlet, Route } from 'react-router-dom';
-import Template from '../layout/Template';
-import Increase from '../pages/Increase';
-import Todos from '../pages/Todos';
+import Template from '@layout/Template';
+import Increase from '@pages/Increase';
+import Todos from '@pages/Todos';
+import Table from '@pages/Table';
 
 const Router = () => {
     const route = createBrowserRouter(
@@ -9,6 +10,7 @@ const Router = () => {
             <Route path='/' element={<Root />}>
                 <Route index element={<Increase />}></Route>
                 <Route path='todos' element={<Todos />}></Route>
+                <Route path='table' element={<Table />}></Route>
             </Route>
         )
     )
