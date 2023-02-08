216ee552bdcc1537b51c6b6980f3cefb00266209
Jakkrirt <Mona Lisa>
2023-02-08 15:09:12 +0700

    feat : update table component

--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -1,11 +1,12 @@
 import Title from "antd/es/typography/Title";
 import TableComponent from '@components/Table/Table';
 import useFetchTable from '@hooks/useFetchTable';
-
+import BreadcrumbSection from '@components/Breadcrumb';
 const TablePage = () => {
     const { tableData } = useFetchTable();
 
     return <>
+        <BreadcrumbSection titlePage='Table datas.' />
         <Title>Table</Title>
         <TableComponent data={tableData} />
     </>
